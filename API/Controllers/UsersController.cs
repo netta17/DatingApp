e94f5c33d50b1ddb0bbb43bce608596fb36c0686
Netta <neat1713@gmail.com>
2023-02-06 16:49:43 +0200

    new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   API/.gitignore 	new file:   API/API.csproj 	new file:   API/Controllers/UsersController.cs 	new file:   API/Controllers/WeatherForecastController.cs 	new file:   API/Data/DataContext.cs 	new file:   API/Data/Migrations/20230131152836_InitialCreate.Designer.cs 	new file:   API/Data/Migrations/20230131152836_InitialCreate.cs 	new file:   API/Data/Migrations/DataContextModelSnapshot.cs 	new file:   API/Entities/AppUser.cs 	new file:   API/Program.cs 	new file:   API/Properties/launchSettings.json 	new file:   API/WeatherForecast.cs 	new file:   API/appsettings.Development.json 	new file:   API/datingapp.db 	new file:   API/global.json 	new file:   API/package-lock.json 	new file:   API/package.json 	new file:   DatingApp.sln 	new file:   client/.editorconfig 	new file:   client/.gitignore 	new file:   client/.vscode/extensions.json 	new file:   client/.vscode/launch.json 	new file:   client/.vscode/tasks.json 	new file:   client/README.md 	new file:   client/angular.json 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/src/app/app-routing.module.ts 	new file:   client/src/app/app.component.css 	new file:   client/src/app/app.component.html 	new file:   client/src/app/app.component.spec.ts 	new file:   client/src/app/app.component.ts 	new file:   client/src/app/app.module.ts 	new file:   client/src/assets/.gitkeep 	new file:   client/src/favicon.ico 	new file:   client/src/index.html 	new file:   client/src/main.ts 	new file:   client/src/styles.css 	new file:   client/ssl/server.crt 	new file:   client/ssl/server.key 	new file:   client/tsconfig.app.json 	new file:   client/tsconfig.json 	new file:   client/tsconfig.spec.json

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.ObjectPool;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] //GET /api/users
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+    }
+}
